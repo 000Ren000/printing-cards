3cd9849d88a2c54ba365d3e3d91abf3ccaec6a6f
000Ren000 <antonren@yandex.ru>
2022-07-29 15:40:20 +0700

    добавил выбор карточек

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,24 @@
 import './Card.css';
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import {useReactToPrint} from 'react-to-print';
 import Form from '../Form/Form';
 
 function Card({shtih, name, functions}) {
   const componentRef = useRef();
+  const [isYellowCard, setIsYellowCard] = useState(false);
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
   });
-
   const handleClick = (evt,) => {
     evt.preventDefault();
     handlePrint();
   }
 
 
-
   return (
     <>
-      <Form functions={{...functions, handleClick}}/>
-      <div className="card" ref={componentRef}>
+      <Form functions={{...functions, handleClick}} color={isYellowCard} handleColor={(isYellow) => setIsYellowCard(isYellow)}/>
+      <div className={`card ${isYellowCard ? 'card_background_yellow' : 'card_background_red'}`} ref={componentRef}>
         <div className="card__name-container">
           <div className="card__name">{name}</div>
         </div>
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Form.css'
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log();
-}
-function Form({functions}) {
-  const {changeName, changeCod, handleClick} = functions
 
+function Form(props) {
+  const {changeName, changeCod, handleClick, } =props.functions;
+  const changeColor = () => {
+    if (!props.color) props.handleColor(true);
+    else props.handleColor(false);
+  }
   return (
    <>
      <form action="src/components/Form/Form" className="form" onSubmit={handleClick}>
@@ -24,6 +24,13 @@ function Form({functions}) {
          placeholder="Последние 4 цифры штрих-кода"
          onChange={(event) => changeCod(event.target.value)}
        />
+       <div className="form__checkbox">
+         <span className='form__label'>Желтая карточка</span>
+         <div className="checkbox">
+           <input className="checkbox__input" type="checkbox" onChange={changeColor}/>
+         </div>
+       </div>
+
        <button className="form__btn">Печать</button>
      </form>
    </>
